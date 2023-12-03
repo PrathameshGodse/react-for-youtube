08722204414d199000f811fb248a5f0c365b36b6
Prathamesh Godse <prathameshgodse00@gmail.com>
2023-12-03 23:06:20 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+
+  const groom= "Salman "
+    const bride= "aish"
+  return (
+    <div>{groom} WedsS {bride}</div>
+  )
+}
+
+export default page
